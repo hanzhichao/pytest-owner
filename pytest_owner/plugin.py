f2f9676f5d9db8183ffa104b40f90175c5d9e4e4
韩志超 <han_zhichao@sina.cn>
2024-08-19 16:16:50 +0800

    fix: 修复指定owner时未排除不带mark.owner标记用例的问题

--- pytest_owner/plugin.py
+++ pytest_owner/plugin.py
@@ -20,16 +20,18 @@ def owner(config):
 
 def pytest_collection_modifyitems(session, config, items):
     selected_owners = config.getoption('--owner')
-    print(selected_owners)
+    # print(selected_owners)
     if selected_owners == None:
         return
     
     deselected_items = set()
     for item in items:
+        marker_owner = None
         for marker in item.iter_markers('owner'):
             [marker_owner] = marker.args
-            if marker_owner not in selected_owners:
-                deselected_items.add(item)
+        if marker_owner not in selected_owners:
+            deselected_items.add(item)
+
     selected_items = [item for item in items if item not in deselected_items]
     items[:] = selected_items
     config.hook.pytest_deselected(items=list(deselected_items))
--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     packages=find_packages(include=['pytest_owner']),
     setup_requires=setup_requirements,
     url='https://github.com/hanzhichao/pytest-owner',
-    version='0.1',
+    version='0.1.1',
     zip_safe=True,
     install_requires=[
         'pytest',
